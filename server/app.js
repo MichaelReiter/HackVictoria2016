d9c3caf0c174abe89e03577817f1346efe0516bf
Erik Reppel <erikreppel@gmail.com>
2016-03-12 14:34:33 -0800

    Add websockets for server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
-// test_app
 var express = require('express');
-var path = require('path')
-var app = express();
+var path = require('path');
+var socketio = require('socket.io');
+var http = require('http');
 
+var app = express();
+var httpServer = http.server(app);
+var io = socketio(httpServer);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+io.on('connection', function(socket) {
+    console.log("connected");
+});
 
 app.get('/', function (req, res) {
   res.sendFile(
-    path.join('static', 'index.html'),
-    {'root': __dirname}
+    path.join(__dirname, 'static/index.html')
   );
 });
 
@@ -15,6 +24,10 @@ app.get('/test_end', function (req, res) {
   res.send('test!');
 });
 
+app.post('/location', function(req, res) {
+    var data = req.body;
+    io.emit('busMetrics', data);
+});
 
 
 app.listen(3000, function () {
