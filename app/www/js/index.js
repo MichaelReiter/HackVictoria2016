483ba1d0faf58138b5c8043d588786ce561eb431
MichaelReiter <michaelreiter@live.ca>
2016-03-12 16:03:06 -0800

    Update bus population method

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,7 +1,7 @@
 var socket = io.connect('http://localhost:3000');
 
 socket.on('busMetrics', function(data) {
-    console.log(data);
+  console.log(data);
 });
 
 var app = {
@@ -51,8 +51,11 @@ var app = {
       var listElement = document.createElement('li');
       listElement.className = 'table-view-cell';
       document.getElementsByClassName('table-view')[0].appendChild(listElement);
-      busNumber = document.createTextNode(busList[bus].number);
-      listElement.appendChild(busNumber);
+      var anchor = document.createElement('a');
+      anchor.className = 'navigate-right';
+      listElement.appendChild(anchor);
+      var busNumber = document.createTextNode(busList[bus].number);
+      anchor.appendChild(busNumber);
     }
   }
 };
@@ -60,5 +63,60 @@ var app = {
 app.initialize();
 
 $(document).ready(function() {
-  setTimeout(function(){ app.initMap(); }, 200);
+  setTimeout(function() {app.initMap();}, 200);
 });
+
+[
+{ number: "1", route: "Downtown/Richardson", hasMetric: false }, 
+{ number: "2/2A", route: "Oak Bay/Willows/Downtown", hasMetric: false }, 
+{ number: "3", route: "Beacon Hill/Gonzales", hasMetric: false }, 
+
+ 
+4 UVic/Downtown
+6 Downtown/Royal Oak Exch
+7 UVic/Downtown
+8 Interurban/Tillicum Mall/Oak Bay
+10 Songhees/Royal Jubilee
+11 Tillicum Mall/UVic
+12 UVic/Univ Heights
+13 Ten Mile Point/UVic
+14 Vic General/UVic
+15 Esquimalt/UVic
+16 Uptown/UVic
+17 Cedar Hill
+19 Hillside Mall
+21 Downtown/Interurban
+22/22n Vic General/Burnside/Hillside Mall/Downtown
+24 Cedar Hill/Admirals Walk
+25 Maplewood/Admirals Walk/Colwood Exch
+26 UVic/Dockyard
+27x/28x Downtown Express/Majestic Express
+27/28 Gordon Head/Majestic/Downtown/Beacon Hill
+30/31 James Bay/Beacon Hill/Royal Oak Exch
+32 Cordova Bay/Royal Oak
+33 UVic
+35 Ridge
+39 Royal Roads/UVic/Royal Oak
+47 Goldstream Mdws/Downtown
+48 Happy Valley/Downtown
+50 Downtown/Langford
+51 Langford/UVic
+52 Colwood/Millstream/Bear Mtn
+53 Langford Exch/Colwood Exch
+54 Metchosin
+55 Happy Valley
+56 Thetis Heights/Langford Exch
+57 Thetis Heights/Westhills
+58 Goldstream Mdws
+59/60 Triangle Mtn/Wishart
+61 Langford/Downtown/Sooke
+63 Otter Point
+64 East Sooke
+70/71/72 Downtown Express/Swartz Bay/Downtown
+75 Royal Oak/Downtown/Saanichton
+76 Swartz Bay/UVic
+81 Swartz Bay/Sidney/Brentwood/Keating X
+83 Royal Oak Exch/Sidney
+85 North Saanich
+88 Airport/Sidney
+]
