6b39e4b7212c4fe19d3849ab4b08169e07b7c6c2
Clement Emmanuel <Clement Emmanuel>
2016-03-12 20:37:40 -0800

    epic merge

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,8 +1,61 @@
 var socket = io.connect('http://localhost:3000');
 
-socket.on('busMetrics', function(data) {
-    console.log(data);
+var busMarkers = [];
+var mapDiv = document.getElementById('map');
+
+setTimeout(function() {initMap();}, 200);
+
+var map = null; 
+
+function initMap() { 
+  map = new google.maps.Map(mapDiv, {
+    center: {lat: 48.45, lng: -123.35},
+    zoom: 12,
+    streetViewControl: false,
+    mapTypeControl: false
+  });
+
+}
+
+socket.on('busInit', function(activeBusList) {
+
 });
+
+function addMarker(id, lat, lng){
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    map: map
+  });
+  marker.setPosition(new google.maps.LatLng(lat, lng));
+  busMarkers.push({id: id, markerObject: marker});
+}
+
+function updateMarker(marker, lat, lng){
+  marker.setPosition(new google.maps.LatLng(lat, lng));
+}
+
+socket.on('updateBus', function(data) {
+  console.log('len of Bus marker List ' + busMarkers.length)
+  console.log('updating bus # ' + data.number );
+
+  var id = data.number;
+  var lat = data.lat;
+  var lng = data.lng;
+
+  var marker = {};
+  for (var i = 0; i < busMarkers.length; i++) {
+    // console.log(busMarkers);
+    if (id === busMarkers[i].id){
+      marker = busMarkers[i].markerObject;
+      updateMarker(marker, lat, lng);
+      return;
+    }
+  };
+
+  addMarker(id, lat, lng);
+
+});
+
 
 var app = {
   // Application Constructor
@@ -35,19 +88,20 @@ var app = {
     console.log('Received Event: ' + id);
   },
 
-  initMap: function() {
-    var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-      center: {lat: 48.45, lng: -123.35},
-      zoom: 12,
-      streetViewControl: false,
-      mapTypeControl: false
-    });
-  }
+  // initMap: function() {
+  //   var mapDiv = document.getElementById('map');
+  //   var map = new google.maps.Map(mapDiv, {
+  //     center: {lat: 48.45, lng: -123.35},
+  //     zoom: 12,
+  //     streetViewControl: false,
+  //     mapTypeControl: false
+  //   });
+  // }
 };
 
 app.initialize();
 
 $(document).ready(function() {
-  setTimeout(function(){ app.initMap(); }, 200);
+
+  // setTimeout(function(){ app.initMap(); }, 200);
 });
